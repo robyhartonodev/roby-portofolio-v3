d92af74038bcec9488906c992aa0d1017b220a08
roby <robyhartonodev312@gmail.com>
2024-02-27 13:53:47 +0100

    update html title tab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import AboutMe from '@/components/aboutme';
 import Project from '@/components/project';
 import Experience from '@/components/experience';
+import Head from "next/head";
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Portofolio - Roby Hartono</title>
+            </Head>
             <div className="space-y-0 md:space-y-16">
                 <AboutMe></AboutMe>
                 <Project></Project>
